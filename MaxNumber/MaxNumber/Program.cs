ce175ae72fea2593f60dac6834f600af609e3449
Puja <pujayadav@gmail.com>
2020-10-20 18:00:25 +0530

    [puja] add . Maximum Value

--- MaxNumber/MaxNumber/Program.cs
+++ MaxNumber/MaxNumber/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace MaxNumber
+namespace MaximumNumberCheck
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Maximum number check program.");
+            Console.WriteLine("Find max Number");
+            MaxNumberCheck MaxInt = new MaxNumberCheck();
+            int result = MaxInt.MaximumIntegerNumber(70, 80, 122);
+            Console.WriteLine(result);
         }
     }
 }
