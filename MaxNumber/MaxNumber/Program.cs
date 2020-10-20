6d2d903f00c9bf03bba90057dd07e16d3943b239
Puja <pujayadav@gmail.com>
2020-10-20 18:05:52 +0530

    [puja] add . Maximum value among float data

--- MaxNumber/MaxNumber/Program.cs
+++ MaxNumber/MaxNumber/Program.cs
@@ -9,8 +9,10 @@ namespace MaximumNumberCheck
             Console.WriteLine("Welcome to Maximum number check program.");
             Console.WriteLine("Find max Number");
             MaxNumberCheck MaxInt = new MaxNumberCheck();
-            int result = MaxInt.MaximumIntegerNumber(70, 80, 122);
+            int result = MaxInt.MaximumIntegerNumber(11, 22, 33);
             Console.WriteLine(result);
+            float floatresult = MaxInt.MaximumFloatNumber(186.8f,100.00f,457.28f);
+            Console.WriteLine(floatresult);
         }
     }
 }
